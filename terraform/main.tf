f434295ff12b415ef4e7f6f6fc8b50b4312f5512
Jingxu Lan <jingxulan98@gmail.com>
2024-12-14 09:24:35 -0500

    Add real api address

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,9 +40,7 @@ resource "aws_instance" "backend" {
   }
 }
 
-resource "aws_api_gateway_rest_api" "api_gateway" {
-  name = "SimpleAPI"
-}
+
 
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
