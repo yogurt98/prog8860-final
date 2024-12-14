10ae914bd4fd6a26be7bfc84c733d45fbe614c41
Jingxu Lan <jingxulan98@gmail.com>
2024-12-14 09:42:20 -0500

    Add gateway

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,9 +40,7 @@ resource "aws_instance" "backend" {
   }
 }
 
-resource "aws_api_gateway_rest_api" "api_gateway" {
-  name = "SimpleAPI"
-}
+
 
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
@@ -76,4 +74,59 @@ resource "aws_lambda_function" "backend_function" {
   filename      = "${path.module}/lambda_function.zip"
 
   source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
+}
+
+# API Gateway REST API
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name        = "MyLambdaAPI"
+  description = "API Gateway for Lambda function"
+}
+
+# API Gateway Resource
+resource "aws_api_gateway_resource" "api_gateway_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "lambda" # This creates the path /lambda
+}
+
+# API Gateway Method
+resource "aws_api_gateway_method" "api_gateway_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+# Lambda Permission for API Gateway
+resource "aws_lambda_permission" "api_gateway_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.backend_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
+}
+
+# API Gateway Integration with Lambda
+resource "aws_api_gateway_integration" "api_gateway_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_resource.api_gateway_resource.id
+  http_method             = aws_api_gateway_method.api_gateway_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.backend_function.invoke_arn
+}
+
+# Deploy the API Gateway
+resource "aws_api_gateway_deployment" "api_gateway_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  depends_on = [
+    aws_api_gateway_integration.api_gateway_integration,
+    aws_api_gateway_method.api_gateway_method,
+  ]
+  stage_name = "prod"
+}
+
+# Output the API Gateway URL
+output "api_gateway_url" {
+  value = "${aws_api_gateway_deployment.api_gateway_deployment.invoke_url}/lambda"
 }
