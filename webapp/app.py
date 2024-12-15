5e25be7f5a1a0341103344d83c4df383c4554a09
Jingxu Lan <jingxulan98@gmail.com>
2024-12-15 02:27:27 -0500

    Delete unnecessary thing

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
-from werkzeug.middleware.proxy_fix import ProxyFix
-from awslambdaric.wsgi import WSGIAdapter
+# from werkzeug.middleware.proxy_fix import ProxyFix
+# from awslambdaric.wsgi import WSGIAdapter
 
 app = Flask(__name__)
 
@@ -9,8 +9,8 @@ def hello_api():
     return jsonify({"message": "Hello, World!"})
 
 # For Lambda: Add WSGI compatibility
-def lambda_handler(event, context):
-    return WSGIAdapter(app)(event, context)
+# def lambda_handler(event, context):
+#     return WSGIAdapter(app)(event, context)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
